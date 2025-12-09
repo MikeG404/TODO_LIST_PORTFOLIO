9c779b11c6c94c4183c8628823c027176c34e5b6
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-12-09 12:43:58 +0100

    add form validation dependencies and remove main page title from home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,6 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center pt-24 bg-gray-100">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex-col">
-        <h1 className="text-4xl font-bold text-blue-600 mb-8">
-          Mon Portfolio: To-Do List
-        </h1>
         <TodoForm />
         <div className="bg-white shadow-xl rounded-lg p-6 w-full max-w-md">
           <TodoList />
