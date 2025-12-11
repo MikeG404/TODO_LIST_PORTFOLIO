5c46fbb5780b0c37866dd56473af83e780746d96
MG404 <mike.godfrinne@gmail.com>
2025-12-11 05:05:08 +0100

    add form handling and validation to LoginPage and SignUpPage components

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,25 @@
+'use client'
+
+import { useForm } from "react-hook-form";
+
+interface ILoginFormInput {
+    email: string;
+    password: string;
+}
+
 export default function LoginPage() {
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { errors } 
+  } = useForm<ILoginFormInput>();
+
+  const onSubmit = (data: ILoginFormInput) => {
+    console.log(data);
+  };
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="email">Email:</label>
         <input type="email" name="email" placeholder="jhon.doe@example.com" />
@@ -9,6 +28,7 @@ export default function LoginPage() {
         <label htmlFor="password">Password:</label>
         <input type="password" name="password" placeholder="*********" />
       </div>
+      <button type="submit">Login</button>
     </form>
   );
 }
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,19 +1,52 @@
+'use client'
+
+import { useForm } from 'react-hook-form';
+
+interface ISignUpFormInput {
+    username: string;
+    email: string;
+    password: string;
+}
+
 export default function SignUpPage() {
+    const { 
+        register, 
+        handleSubmit, 
+        formState: { errors } 
+    } = useForm<ISignUpFormInput>();
+
+    const onSubmit = (data: ISignUpFormInput) => {
+        console.log(data);
+    }
+
 
     return (
-        <form action="">
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <label htmlFor="username">Username:</label>
-                <input type="text" name="username" placeholder="JD404"/>
+                <input 
+                    type="text" 
+                    placeholder="JD404"
+                    {...register('username')}
+                    />
             </div>
             <div>
                 <label htmlFor="email">Email:</label>
-                <input type="email" name="email" placeholder="jhon.doe@example.com"/>
+                <input 
+                    type="email" 
+                    placeholder="jhon.doe@example.com"
+                    {...register('email')}      
+                    />
             </div>
             <div>
                 <label htmlFor="password">Password:</label>
-                <input type="password" name="password" placeholder="*********" />
+                <input 
+                    type="password" 
+                    placeholder="*********" 
+                    {...register('password')}
+                    />
             </div>
+            <button type='submit'>Sign Up</button>
         </form>
     );
 }
